e998d7ecd7ea2b5a1758abb8daf91f207136de15
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2026-02-07 01:22:11 -0500

    linear and double interpolation functions

--- steam_tabler.py
+++ steam_tabler.py
@@ -13,6 +13,16 @@ def read_csv(filepath):
         for i in range(title_block_num_lines):
             next(f)
         return list(DictReader(f))
+    
+def lin_interpolate(x, x_min, x_max, y_min, y_max):
+    if x_min == x_max:
+        raise ArithmeticError("Divide by Zero when interpolating!")
+    return y_min + ((x - x_min) * (y_max - y_min)) / (x_max - x_min)
+
+def double_interpolate(x, y, x_min, x_max, y_min, y_max, z_x_y, z_X_y, z_x_Y, z_X_Y):
+    low_y_point = lin_interpolate(x, x_min, x_max, z_x_y, z_X_y)
+    high_y_point = lin_interpolate(x, x_min, x_max, z_x_Y, z_X_Y)
+    return lin_interpolate(y, y_min, y_max, low_y_point, high_y_point)
 
 sat_by_P_file = os.path.join(dir_path, "saturated_by_pressure_V1.4.csv")
 sat_by_T_file = os.path.join(dir_path, "saturated_by_temperature_V1.5.csv")
