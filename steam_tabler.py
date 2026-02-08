772ba85baeef7cbd1079fca2012fbeb25c4df091
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2026-02-08 18:46:23 -0500

    Enum for property keys and number conversion

--- steam_tabler.py
+++ steam_tabler.py
@@ -1,9 +1,32 @@
 from csv import DictReader
 import os
+from enum import Enum
 
 # this doesn't work if the working directory has been changed
 # so it's at the beginning just in case
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
+class Property(Enum):
+    TEMP = 'T (°C)'
+    PRESSURE = 'P (MPa)'
+    VOLUME_LIQUID = 'Specific Volume Liquid (m^3/kg)'
+    VOLUME_VAPOR = 'Specific Volume Vapor (m^3/kg)'
+    VOLUME = 'Specific Volume (m^3/kg)'
+    DENSITY = 'Density (kg/m^3)'
+    ENERGY_LIQUID = 'Internal Energy Liquid (kJ/kg)'
+    ENERGY_VAPOR = 'Internal Energy Vapor (kJ/kg)'
+    ENERGY_VAPORIZATION = 'Internal Energy of Vaporization (kJ/kg)'
+    ENERGY = 'Specific Internal Energy (kJ/kg)'
+    ENTHALPY_LIQUID = 'Enthalpy Liquid (kJ/kg)'
+    ENTHALPY_VAPOR = 'Enthalpy Vapor (kJ/kg)'
+    ENTHALPY_VAPORIZATION = 'Enthalpy of Vaporization (kJ/kg)'
+    ENTHALPY = 'Specific Enthalpy (kJ/kg)'
+    ENTROPY_LIQUID = 'Entropy Liquid [kJ/(kg K)]'
+    ENTROPY_VAPOR = 'Entropy Vapor [kJ/(kg K)]'
+    ENTROPY_VAPORIZATION = 'Entropy of Vaporization [kJ/(kg K)]'
+    ENTROPY = 'Specific Entropy [kJ/(kg K)]'
+    PHASE = 'Phase'
+
 
 def read_csv(filepath):
     title_block_num_lines = 6
@@ -13,6 +36,15 @@ def read_csv(filepath):
         for i in range(title_block_num_lines):
             next(f)
         return list(DictReader(f))
+    
+def convert_to_number(list_dict_table: list[dict]):
+    for x in list_dict_table:
+        for key in x.keys():
+            try:
+                x[key] = float(x[key])
+            except ValueError:
+                continue
+    return list_dict_table
     
 def lin_interpolate(x, x_min, x_max, y_min, y_max):
     if x_min == x_max:
@@ -28,6 +60,8 @@ sat_by_P_file = os.path.join(dir_path, "saturated_by_pressure_V1.4.csv")
 sat_by_T_file = os.path.join(dir_path, "saturated_by_temperature_V1.5.csv")
 comp_sup_file = os.path.join(dir_path, "compressed_liquid_and_superheated_steam_V1.3.csv")
 
-sat_by_T = read_csv(sat_by_T_file)
-sat_by_P = read_csv(sat_by_P_file)
-comp_sup = read_csv(comp_sup_file)
+sat_by_T = convert_to_number(read_csv(sat_by_T_file))
+sat_by_P = convert_to_number(read_csv(sat_by_P_file))
+comp_sup = convert_to_number(read_csv(comp_sup_file))
+
+print([x for x in sat_by_P if x["P (MPa)"] == 1.3])
