1759907a80f17653e28fa1fb00963e3af934210f
WesleyJ-128 <wesley.johnson.4096@gmail.com>
2026-02-09 09:05:57 -0500

    moved number conversion into csv read function

--- steam_tabler.py
+++ steam_tabler.py
@@ -39,16 +39,14 @@ def read_csv(filepath):
         # throw away non-data lines
         for i in range(title_block_num_lines):
             next(f)
-        return list(DictReader(f))
-    
-def convert_to_number(list_dict_table: list[dict]):
-    for x in list_dict_table:
-        for key in x.keys():
-            try:
-                x[key] = float(x[key])
-            except ValueError:
-                continue
-    return list_dict_table
+        dict_list = list(DictReader(f))
+        for x in dict_list:
+            for key in x.keys():
+                try:
+                    x[key] = float(x[key])
+                except ValueError:
+                    continue
+        return dict_list
     
 def lin_interpolate(x, x_min, x_max, y_min, y_max):
     if x_min == x_max:
@@ -64,6 +62,6 @@ sat_by_P_file = os.path.join(dir_path, "saturated_by_pressure_V1.4.csv")
 sat_by_T_file = os.path.join(dir_path, "saturated_by_temperature_V1.5.csv")
 comp_sup_file = os.path.join(dir_path, "compressed_liquid_and_superheated_steam_V1.3.csv")
 
-sat_by_T = convert_to_number(read_csv(sat_by_T_file))
-sat_by_P = convert_to_number(read_csv(sat_by_P_file))
-comp_sup = convert_to_number(read_csv(comp_sup_file))
+sat_by_T = read_csv(sat_by_T_file)
+sat_by_P = read_csv(sat_by_P_file)
+comp_sup = read_csv(comp_sup_file)
